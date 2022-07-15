fd145ed29e5506c89aa87b6e31a84ae22b6c85e8
yeonnnee <yeonnnee@gmail.com>
2022-07-15 20:49:52 +0900

    ConfirmModal 생성

---
+++ src/components/modals/ConfirmModal.tsx
@@ -0,0 +1,33 @@
+import Button from "../Button";
+import '../../styles/components/_confirm-modal.scss';
+
+interface ConfirmModalProps {
+  isActive:boolean,
+  text: string,
+  onConfirm:() => void,
+  onCancel:() => void
+}
+
+export default function ConfirmModal(props: ConfirmModalProps) {
+  const {isActive, text, onConfirm, onCancel} = props;
+
+  return(
+    <>
+      {
+        isActive ? 
+        <div className="confirm-modal">
+          <div className="close-btn">
+            <Button text={'X'} type={"cancel"} shape={"circle"} onClick={onCancel}/>
+          </div>
+          <p className="message">{text}</p>
+          <div className="buttons">
+            <Button text={'취소'} type={"cancel"} shape={"circle"} onClick={onCancel}/>
+            <Button text={'확인'} type={"confirm"} shape={"circle"} onClick={onConfirm}/>
+          </div>
+        </div> 
+        : 
+        null  
+      }
+    </> 
+  )
+}

f93713cc7063e056ec2c6766decf8e50065ad57e
yeonnnee <yeonnnee@gmail.com>
2022-07-15 19:45:39 +0900

    remove unused variable and line

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import More from './More';
-import DropDown from './More';
+
 
 export interface ListItemProps {
   title: string,
@@ -9,17 +9,7 @@ export interface ListItemProps {
 
 export default function ListItem(props: ListItemProps) {
   const {title, id} = props;
-  const options = [
-    {
-      text: '수정하기',
-      value: 'edit'
-    },
-    {
-      text: '삭제하기',
-      value: 'delete'
-    }
-  ];
-  
+
   return (
     <li className="item">
       <img src="https://place-hold.it/300" alt={title}/>

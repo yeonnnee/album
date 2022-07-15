65f570282d274e280be8d1cb6ca42995707363d1
yeonnnee <yeonnnee@gmail.com>
2022-07-15 19:45:02 +0900

    remove unused line

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,6 +1,6 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { SetStateAction } from 'react';
+
 
 
 interface TextInputProps {

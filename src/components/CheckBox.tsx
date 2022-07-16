0e48fa161896a83a6e0a5941dd876ebef0fa535b
yeonnnee <yeonnnee@gmail.com>
2022-07-16 19:01:33 +0900

    체크박스 컴포넌트 생성

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import { MutableRefObject, useRef } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
+import '../styles/components/_checkbox.scss';
+
+interface CheckBoxProps {
+  labelText?: string,
+  id: string,
+  onClick: () => void
+}
+
+export default function CheckBox(props: CheckBoxProps) {
+  const {labelText, id, onClick} = props;
+  const albums = useSelector((state:RootState) => state.album);
+  const selectedAlbums = albums.searchResult.filter(x => x.isChecked);
+  const targetId = labelText !== 'Select All' && +id.split('-')[1];
+  const targetStatus = targetId && albums.searchResult.filter(x => x.id === targetId)[0].isChecked;
 
 
-export default function CheckBox () {
+  return(
+    <>
+      <input id={id} type="checkbox" checked={labelText === 'Select All' ? selectedAlbums.length === 5 : targetStatus ? targetStatus : false} onChange={()=>onClick()}/>
+      <label className="checkbox" htmlFor={id}>{labelText ? labelText : null}</label>
+    </>
 
+  )
 }

a2e1e3534ad8413ea9622201ec787e886d244212
yeonnnee <yeonnnee@gmail.com>
2022-07-16 11:33:06 +0900

    image loader 셍상

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -10,7 +10,10 @@ export default function ListItem(props: ListItemProps) {
 
   return (
     <li className="item">
-      <img src="https://place-hold.it/300" alt={title}/>
+      <div className='image-wrapper'>
+        <p>Loading...</p>
+        <img src="https://place-hold.it/300" alt={title}/>
+      </div>
       <div className='image-title'>
         <p className='title'>{title}</p>
         <More title={title} id={id.toString()}/>

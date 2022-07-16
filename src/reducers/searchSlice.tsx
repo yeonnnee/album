9d08f425a68428725fc318460643a8721417a143
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2022-07-16 20:27:19 +0900

    search 조건 reducer 생성

---
+++ src/reducers/searchSlice.tsx
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+
+export interface SearchState {
+  searchString: string
+};
+
+const initialState: SearchState = {
+  searchString: ''
+};
+
+
+const searchSlice = createSlice({
+  name: 'search',
+  initialState,
+  reducers: {
+    setSearchString: (state, action: PayloadAction<string>) => {
+      return state = {
+        ...state,
+        searchString: action.payload
+      }
+    },
+    resetSearchCondition: (state) => {
+      return state = {
+        ...state,
+        searchString: ''
+      }
+    }
+  }
+});
+
+
+  export const { setSearchString, resetSearchCondition } = searchSlice.actions;
+  export default searchSlice.reducer;
--- src/store.tsx
+++ src/store.tsx
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import albumReducer from "./reducers/albumSlice";
+import searchReducer from "./reducers/searchSlice";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { albumApi } from "./services/albums";
 
 export const store = configureStore({
   reducer: {
     [albumApi.reducerPath]: albumApi.reducer,
-    album: albumReducer
+    album: albumReducer,
+    searchCondition: searchReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(albumApi.middleware),
 })
